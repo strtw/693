83ea72ecfe2ab03b28ad52ef8381c7413cd7db7e
Stu Wood <souporstu@gmail.com>
2023-06-10 09:29:08 -0700

    Module-4-starter

--- module-3/app.js
+++ module-3/app.js
@@ -1,13 +1,25 @@
-import express from 'express';
+import express from 'express'
+import {} from 'dotenv/config'
+import routes from './routes/routes.js'
+import connectDB from './db/connect.js'
+import bodyParser from 'body-parser'
 
-const app = express();
+const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use('/', routes)
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: 'public' });
-});
+const PORT = process.env.PORT || 5000
 
-app.listen(3000, () => {
-  console.log('App is listening on port 3000');
-});
+const init = async () => {
+    try {
+        await connectDB(process.env.DB)
+        console.log('Connected to the database...')
+        app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+init()
---
+++ module-3/public/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+var _employees = _interopRequireDefault(require("./employees"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+_reactDom["default"].render( /*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, null, /*#__PURE__*/_react["default"].createElement(_employees["default"], null)), document.getElementById('content'));
